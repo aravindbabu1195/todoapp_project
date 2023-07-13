9067bd86fb03c913d0668e22c36b072b55dced99
Aravind Babu <aravindbabu1195@gmail.com>
2023-07-13 11:51:14 +0530

    added

---
+++ todoapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from todoapp import views
+
+urlpatterns = [
+
+    path('', views.add, name='add'),
+    path('delete/<int:taskid>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('listview/', views.Tasklistview.as_view(), name='listview'),
+    path('detailview/<int:pk>/', views.Taskdetailview.as_view(), name='detailview'),
+    path('updateview/<int:pk>/', views.Taskupdateview.as_view(), name='updateview'),
+    path('deleteview/<int:pk>/', views.Taskdeleteview.as_view(), name='deleteview'),
+]
---
+++ todoapp/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
+from .models import Task
+from todoapp.forms import TodoForm
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView,DeleteView
+
+class Tasklistview(ListView):
+    model = Task
+    template_name = 'listview.html'
+    context_object_name = 'task'
+
+
+class Taskdetailview(DetailView):
+    model = Task
+    template_name = 'details.html'
+    context_object_name = 'i'
+
+class Taskupdateview(UpdateView):
+    model = Task
+    template_name = 'update.html'
+    context_object_name = 'i'
+    fields = ('name', 'priority', 'date')
+
+    def get_success_url(self):
+        return reverse_lazy('detailview',kwargs={'pk':self.object.id})
+
+class Taskdeleteview(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    success_url = reverse_lazy('listview')
+
+# Create your views here.
+def add(request):
+    task1 = Task.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('task', '')
+        priority = request.POST.get('priority', '')
+        date = request.POST.get('date', '')
+        task = Task(name=name, priority=priority, date=date)
+        task.save()
+    return render(request, "home.html", {'task': task1})
+
+
+def delete(requset, taskid):
+    if requset.method == 'POST':
+        task = Task.objects.get(id=taskid)
+        task.delete()
+        return redirect('/')
+    return render(requset, "delete.html")
+
+
+def update(requset, id):
+    task = Task.objects.get(id=id)
+    f = TodoForm(requset.POST or None, instance=task)
+    if f.is_valid():
+        f.save()
+        return redirect('/')
+    return render(requset, "edit.html", {'f': f, 'task': task})
